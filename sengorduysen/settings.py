d5eb5ce69ad84440228dd63a9318f6217f7fc843
ibrhmvci <avci-ibrahim@hotmail.com>
2017-08-29 13:34:37 +0300

    settings changed

--- sengorduysen/settings.py
+++ sengorduysen/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'sengorduysen.wsgi.application'
 
 DATABASES = {
    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'app_data',
     }
  }
